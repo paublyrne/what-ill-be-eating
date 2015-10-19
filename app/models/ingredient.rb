235449c8376b8691821235c8921491140ea6166d
paulgbyrne <paulgregorybyrne@gmail.com>
2015-10-19 19:46:54 +0200

    An ingredient knows when is gone

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,15 @@
 class Ingredient
-  #code
+  attr_accessor :quantity
+  def initialize(options = {})
+    @gone = false
+    @quantity = options[:quantity]
+  end
+
+  def use_some(amount)
+    self.quantity = self.quantity - amount
+  end
+
+  def gone?
+    self.quantity === 0
+  end
 end
