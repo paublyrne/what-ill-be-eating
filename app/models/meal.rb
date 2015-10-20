3c2bca5b329928ef5e9b0f895623902331b2eccd
paulgbyrne <paulgregorybyrne@gmail.com>
2015-10-20 15:10:59 +0200

    Add meal model.\nA new meal is not made.

---
+++ app/models/meal.rb
@@ -0,0 +1,10 @@
+class Meal
+  def initialize
+    @made = false
+  end
+
+  def made?
+    @made
+  end
+
+end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-# RSpec.describe Meal do
-  # it "knows it can be made when all its ingredients are present in sufficient quantities" do
-  #   meal = Meal.new
-  #   ingredient = Ingredient.new(
-  #     quantity: 500, name: "Pasta")
-  #   anotherIngredient = Ingredient.new(
-  #     quantity: 500, name: "Tomatos")
-  #   ingredient.use_some(500)
-  #   expect(ingredient).to be_gone
-  # end
-# end
+RSpec.describe Meal do
+  describe "initialization" do
+    let(:meal) { Meal.new }
+
+    it "knows that a new meal not to be made" do
+      expect(meal).not_to be_made
+    end
+
+  end
+end
