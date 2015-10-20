d911f67de1249a869f406d090421afaaa0ac692a
paulgbyrne <paulgregorybyrne@gmail.com>
2015-10-20 15:28:14 +0200

    Meal spec:It knows that when a meal has been made, it is made.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,10 +1,15 @@
 class Meal
+  attr_accessor :made
   def initialize
     @made = false
   end
 
   def made?
-    @made
+    made
+  end
+
+  def make
+    self.made = true
   end
 
 end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -4,8 +4,13 @@ RSpec.describe Meal do
   describe "initialization" do
     let(:meal) { Meal.new }
 
-    it "knows that a new meal not to be made" do
+    it "knows that a new meal is not made" do
       expect(meal).not_to be_made
+    end
+
+    it "knows that a meal that has been made is made" do
+      meal.make
+      expect(meal).to be_made
     end
 
   end
